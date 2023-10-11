7983ab4ef2a8f56a276405bf28fcff2b7080fa1e
Silas Knobel <silas.knobel@katun.ch>
2023-10-11 16:42:48 +0200

    added cloudwatch policy to ec2-role

--- elasticbeanstalk/iam-instance.tf
+++ elasticbeanstalk/iam-instance.tf
@@ -20,7 +20,6 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = aws_iam_role.ec2.name
 }
 
-
 resource "aws_iam_policy_attachment" "beanstalk_ec2_worker" {
   name       = "${var.applicationName}-elastic-beanstalk-ec2-worker"
   roles      = ["${aws_iam_role.ec2.id}"]
@@ -37,4 +36,42 @@ resource "aws_iam_policy_attachment" "beanstalk_ec2_container" {
   name       = "${var.applicationName}-elastic-beanstalk-ec2-container"
   roles      = ["${aws_iam_role.ec2.id}"]
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
+}
+
+## ==== CloudWatch Logs ====
+resource "aws_iam_policy" "cloudwatch" {
+  name        = "${var.applicationName}-cloudwatch-policy"
+  path        = "/"
+  description = "Allows CloudWatch Logs to publish log data to a log group"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "CloudWatchLogsAccess",
+        "Action" : [
+          "logs:CreateExportTask",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:DescribeDestinations",
+          "logs:DescribeExportTasks",
+          "logs:DescribeLogGroups",
+          "logs:FilterLogEvents",
+          "logs:PutDestination",
+          "logs:PutDestinationPolicy",
+          "logs:PutLogEvents",
+          "logs:PutMetricFilter"
+        ],
+        "Effect" : "Allow",
+        "Resource" : [
+          "arn:aws:logs:*:*:log-group:*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "cloudwatch" {
+  name       = "${var.applicationName}-cloudwatch"
+  roles      = ["${aws_iam_role.ec2.id}"]
+  policy_arn = aws_iam_policy.cloudwatch.arn
 }
